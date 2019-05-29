62ce7b1835a902d6903c3dc2d1abd707e705a56d
ryaan-anthony <rtulino@gmail.com>
2019-05-28 23:01:43 -0400

    test match percent

--- test/poker_slots/services/line_matcher_test.rb
+++ test/poker_slots/services/line_matcher_test.rb
@@ -10,5 +10,11 @@ module PokerSlots
     def test_valid_line
       assert(LineMatcher.new([[1, 1], [2, 2], [3, 3]]))
     end
+
+    def test_match_percent
+      assert_equal(LineMatcher.new([[1, 2], [1, 2], [1, 2]]).match_percent, 1.0)
+      assert_equal(LineMatcher.new([[1, 2], [1, 3], [1, 4]]).match_percent, 0.5)
+      assert_equal(LineMatcher.new([[1, 4], [2, 4], [3, 4]]).match_percent, 0.5)
+    end
   end
 end
